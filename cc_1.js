be2404fc3365d31051308793a411c7a245d63f38
Dylan <safirodylan@gmail.com>
2025-01-25 23:02:23 -0500

    Task 3 - Financial Transactions

--- cc_1.js
+++ cc_1.js
@@ -15,3 +15,19 @@ var isAvailable = true // Product Availability
 console.log("Product Name:", productName, "|", "Type:", typeof productName)
 console.log("Product Price:", productPrice, "|", "Type:", typeof productPrice)
 console.log("Is Available:", isAvailable, "|", "Type:", typeof isAvailable)
+
+// Task 3: Financial Transactions
+let accountBalance = 12000 // Initial account balance in dollars
+    console.log("Initial Account Balance", accountBalance)
+
+accountBalance += 6000 // Balance after a deposit of $6000
+    console.log("Account Balance After Deposit:", accountBalance)
+
+accountBalance -= 9500 // Balanace after withdrawl of $9500
+    console.log("Account Balance After Withdrawl:", accountBalance)
+
+accountBalance *= 1.0075 // Balance after an accured interest rate of 7.5%
+    console.log("Account Balance After Accrued Interest:", accountBalance.toFixed(2))
+
+accountBalance /= 1.125 // Balance after a depreciation rate of 12.5%
+    console.log("Account Balance After Depreciation:", accountBalance.toFixed(2))
