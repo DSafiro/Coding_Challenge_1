06bb4c2ce37a2bf70c8b5acf10933b7598a94047
Dylan <safirodylan@gmail.com>
2025-01-25 22:53:11 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+// Task 1: Employee Information
+let employeeName = "Billy Bob" // Employee's Name
+const employeeID = 8675309 // Employee's ID
+var isActive = true // Employee Activity
+
+console.log("Employee Name:", employeeName, "|", "Type:", typeof employeeName)
+console.log("EmployeeID:", employeeID, "|", "Type:", typeof employeeID)
+console.log("Is Active:", isActive, "|", "Type:", typeof isActive)
