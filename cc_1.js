2539d0f01ba7a936c4a80195a78417dbc5888985
Dylan <safirodylan@gmail.com>
2025-01-25 22:58:05 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -6,3 +6,12 @@ var isActive = true // Employee Activity
 console.log("Employee Name:", employeeName, "|", "Type:", typeof employeeName)
 console.log("EmployeeID:", employeeID, "|", "Type:", typeof employeeID)
 console.log("Is Active:", isActive, "|", "Type:", typeof isActive)
+
+// Task 2: Product Details
+let productName = "Grilled Cheese Sandwich" // Product Name
+const productPrice = 15.99 // Product Price (In Dollars)
+var isAvailable = true // Product Availability
+
+console.log("Product Name:", productName, "|", "Type:", typeof productName)
+console.log("Product Price:", productPrice, "|", "Type:", typeof productPrice)
+console.log("Is Available:", isAvailable, "|", "Type:", typeof isAvailable)
